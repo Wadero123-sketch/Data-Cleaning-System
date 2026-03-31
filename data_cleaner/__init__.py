7acbacf15925fc9038907ecd1686cc5bea35cb07
Wadero123-sketch <93657878+Wadero123-sketch@users.noreply.github.com>
2026-03-31 13:34:54 +0300

    Delete data_cleaner directory

--- data_cleaner/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data Cleaner Package"""
-from .cleaner import DataCleaner
-from .validators import DataValidator
-from .preprocessors import DataPreprocessor
-
-__all__ = ['DataCleaner', 'DataValidator', 'DataPreprocessor']
